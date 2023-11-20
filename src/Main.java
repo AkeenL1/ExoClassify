dd358f779cffd07e888d3411417c5f7fa1f6e664
akeenl-sp <akeen.lewis@smartrent.com>
2023-11-19 20:02:20 -0500

    Project Initialization

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+// First, complete all the stuff in here
+// https://github.com/florinpop17/app-ideas/blob/master/Projects/3-Advanced/NASA-Exoplanet-Query.md
+// Second adjust it to finish these from the notion
+// https://chat.openai.com/c/9ea3b816-f9fd-4fa2-bbea-534e0e9349e3
+// Need to make a public GitHub for it
+public class Main {
+    public static void main(String[] args) {
+        // Empty right now for initialization
+    }
+}
